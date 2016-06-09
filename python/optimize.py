e0296a2f664fb245bf72b544305b64943b99b307
Michael Souza <souza.michael@gmail.com>
2016-06-09 09:23:42 -0300

    Add verbose option to powell method and test_powell

--- python/optimize.py
+++ python/optimize.py
@@ -3,8 +3,8 @@ import numpy as np
 
 def bracket(f,x1,h):
      ''' a,b = bracket(f,xstart,h)
-         Finds the brackets (a,b) of minimum point of the user-supplied 
-         scalar function f(x). The search downhill from xstart with a 
+         Finds the brackets (a,b) of minimum point of the user-supplied scalar 
+         function f(x). The search downhill from xstart with a 
          step length h.
      '''  
      c = 1.618033989
@@ -26,7 +26,7 @@ def bracket(f,x1,h):
      raise Exception('Bracket did not find a minimum')
 
 def golden(f,a,b,tol=1E-03):
-     ''' x,fmin = search(f,a,b,tol=1E-06)
+     ''' x,fmin = golden(f,a,b,tol=1E-06)
          Golden search method for determining x that minimizes the user-suplied
          scalar function f(x).
          The minimum must be bracketed in (a,b).
@@ -49,42 +49,69 @@ def golden(f,a,b,tol=1E-03):
                x1 = R*a + C*b; f1 = f(x1)
      if(f1 < f2): return x1,f1,i
      else: return x2,f2,i
-
-def powell(F,x,h=0.1,tol=1E-06,verbose=False):
-     def f(s): return F(x + s * v)   # F in direction of v
-
+     
+def powell(F,x,h=0.1,tol=1E-06,search='golden',verbose=False):
+     def f(s): return F(x + s * v)   # F in direction of v     
      n  = len(x)                     # number of variables
      df = np.zeros(n,dtype=float)    # decreases of F
      u  = np.eye(n,dtype=float) # vectors v are stored by row
+
+     if verbose:
+          print 'Init -----------'
+          print 'F(x)  = ', F(x)
+          print 'x     = ', x
+
+     # set search algorithm
+     if search is 'golden':
+          search = golden     
+     else:
+          raise Exception('Unsupported linear search algorithm %s'%(search))
+
+     print 'Steps ----------'
      for j in range(30):
           xold = x.copy()
           fold = F(xold)
-          print('iter %5d F(x) = % g'%(j, fold))
+          if verbose: print 'iter %5d F(x) = % g'%(j, fold)
           
           # first n line searches record decreases of F
           for i in range(n):
-               v      = u[i]
-               a,b    = bracket(f,0.0,h)
-               s,fmin,niter = search(f,a,b)
-               df[i]  = fold - fmin
-               fold   = fmin
-               x      = x + s * v
-               print('  search direcion %2d niters = %2d df = % g'%(i,niter,df[i]))
+               v = u[i]
+               a, b = bracket(f,0.0,h)
+               s, fmin, niter = search(f, a, b)
+               df[i] = fold - fmin
+               fold = fmin
+               x = x + s * v
+               if verbose: print '  search direcion %2d niters = %2d df = % g'%(i,niter,df[i])
           
           # last line search in the cycle
-          v   = x - xold
+          v = x - xold
           a,b = bracket(f,0.0,h)
-          s,flast,_ = search(f,a,b)
+          s, flast, _ = search(f,a,b)
           x = x + s * v
 
           # check convergence
           if np.linalg.norm(x - xold)/n < tol:
-              return x, j+1, 'powell converged after %d iterations'%(j+1)
+              msg = 'powell converged after %d iterations'%(j+1)
+              if verbose:
+                   print 'Final -----------'
+                   print 'F(x)  = ', fold
+                   print 'x     = ', x
+                   print 'niter = ', j+1
+                   print 'msg   = ', msg
+              return x, j+1, msg
 
           # identify biggest decrease & update v's
           imax = int(np.argmax(df))
           for i in range(imax, n-1):
-               u[i]   = u[i+1]
+               u[i] = u[i+1]
                u[n-1] = v
      raise Exception('Powell method did not converge')
-'
+
+
+def __test_powell():
+  F = lambda x: 100 * (x[0] - x[1]**2)**2 + (1 - x[0])**2
+  xstart = np.array([3, 9])
+  xmin, niter, msg = powell(F, xstart, verbose=True)  
+
+if __name__ == '__main__':
+  __test_powell()
